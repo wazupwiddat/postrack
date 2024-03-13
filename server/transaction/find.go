85332bd1d5b6e3d51cd5175321b29e29ed4c5858
wazupwiddat <wazupwiddat@gmail.com>
2024-03-13 17:59:58 -0500

    added a view for transactions

--- server/transaction/find.go
+++ server/transaction/find.go
@@ -13,3 +13,13 @@ func FindAllByUser(db *gorm.DB, u *user.User) ([]Transaction, error) {
 	}
 	return transactions, nil
 }
+
+func FindAllByUserPaginated(db *gorm.DB, u *user.User, page int, pageSize int) ([]Transaction, error) {
+	var transactions []Transaction
+	offset := (page - 1) * pageSize
+	res := db.Offset(offset).Limit(pageSize).Find(&transactions, &Transaction{UserID: u.ID})
+	if res.Error != nil {
+		return nil, res.Error
+	}
+	return transactions, nil
+}
